2ea5f5ccc25065f6af4e460944bda10b8b1ffc7c
monica-suarez <shutterbugmo@gmail.com>
2020-04-14 16:24:20 -0500

    enabled js resume

---
+++ resumePage/resume-interact.js
@@ -0,0 +1,48 @@
+const showSkills = () => {
+let x = document.getElementById("skills");
+if (x.style.display === "none" || x.style.display == '') {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+const showExhibitions= () => {
+let x = document.getElementById("exhibitions");
+if (x.style.display === "none" || x.style.display == '') {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+} 
+const showFilms = () => {
+let x = document.getElementById("films");
+if (x.style.display === "none" || x.style.display == '') {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+const showPublications = () => {
+let x = document.getElementById("publications");
+if (x.style.display === "none" || x.style.display == '') {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+const showOther = () => {
+let x = document.getElementById("other");
+if (x.style.display === "none" || x.style.display == '') {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+const showEducation = () => {
+let x = document.getElementById("education");
+if (x.style.display === "none" || x.style.display == '') {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
